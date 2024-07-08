e14f535479fb03a8f2d006fbe32e9918204efcb7
YashDxr <yashdxr@gmail.com>
2024-07-08 19:42:52 +0530

    new class

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Animal animal = new Animal();
+
         System.out.println("Hello from java file for Jenkins...");
+    
+        System.out.println(animal.name);
     }
 }
